c74b4959758fea3e2686c2d877f5d14273f610d2
Кулицкий <@ASU-2.ZSK.BY>
2013-11-20 22:42:16 +1100

    Finished all Templates. Added JS actions. Ready to be knitted with Java backend

--- public_html/js/retail.js
+++ public_html/js/retail.js
@@ -4,9 +4,34 @@
 
 /* Support */
 $("#slist a").click(function(e) {
-   e.preventDefault(); 
-   $(this).next("p").toggle(200);
-   return false;
+    e.preventDefault();
+    $(this).next("p").toggle(200);
+    return false;
+});
+
+$("#categories-menu li.has-sub").click(function() {
+    $(this).find("ul").each(function() {
+        $(this).slideToggle();
+    });
+    $(this).find("span").toggleClass("glyphicon-plus-sign").toggleClass("glyphicon-minus-sign");
+});
+
+$(document).ready(function() {
+    alert("1-2-3");
+    $(".totop").hide();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 600) {
+            $('.totop').slideDown();
+        } else {
+            $('.totop').slideUp();
+        }
+    });
+
+    $('.totop a').click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({scrollTop: 0}, 500);
+    });
 });
 
 
